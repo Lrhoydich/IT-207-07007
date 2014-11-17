7d96c728f75ef7150c6b07c0a201c92affd85730
unknown <luke.hoydich@ACC-GAME-11.lab.snhu.edu>
2014-11-17 13:41:56 -0500

    initial project commit

---
+++ Assets/scripts/mycharactercontroller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mycharactercontroller : MonoBehaviour { 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = Vector3.zero;
+
+		// is up pressed?
+		if (Input.GetKey (KeyCode.UpArrow) == true) {
+			// move character up by some amount
+			direction.y += 0.1f;
+		}
+		// is left pressed?
+		if (Input.GetKey (KeyCode.LeftArrow) == true) {
+			// move character up by some amount
+			direction.x -= 0.1f;
+		}
+		// is right pressed?
+		if (Input.GetKey (KeyCode.RightArrow) == true) {
+			// move character right by some amount
+			direction.x += 0.1f;
+		}
+		// is down pressed?
+		if (Input.GetKey (KeyCode.DownArrow) == true) {
+			// move character down by some amount
+			direction.y -= 0.1f;
+		}
+
+		gameObject.transform.Translate (direction);
+
+	}
+}
